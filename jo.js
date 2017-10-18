27ac2a5f20dcf9fd6c034d69d81b6a7c21f966ae
Yash Ketkar <yash.ketkar@cerner.com>
2017-10-17 22:24:25 -0500

    Align chairs

--- jo.js
+++ jo.js
@@ -6,7 +6,9 @@ WebFontConfig = {
     //  'active' means all requested fonts have finished loading
     //  We set a 1 second delay before calling 'createText'.
     //  For some reason if we don't the browser cannot render the text the first time it's created.
-    active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
+
+    // active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
+
     //  The Google Fonts we want to load (specify as many as you like in the array)
     google: {
       families: ['Source Sans Pro', 'serif']
@@ -22,124 +24,48 @@ function preload() {
 var cursors;
 
 function create() {
-  //  Make our game world 2000x2000 pixels in size (the default is to match the game size)
-  // game.world.setBounds(0, 0, 3000, 800);
-
-
-
   game.stage.backgroundColor = "#FFFFFF";
 
   var xplus = 400;
   var yplus = 400;
-
   var test1 = game.add.sprite(xplus+0, yplus+0, 'chair');
   test1.scale.setTo(0.05, 0.05);
+  test1.x=400-15;
+  test1.y=400-15;
 
-  var r = 100;
-  var x = r*Math.cos(Math.PI/4);
-  var y = r*Math.cos(Math.PI/4);
-  console.log(x);
-console.log(y);
-
-
-  var test = game.add.sprite(xplus+x, yplus+y, 'chair');
-  test.scale.setTo(0.05, 0.05);
-
-  // game.camera.x += 400;
-  // game.camera.y += 400;
-
-  //  Set the scale of the sprite to the random value
+  var s=[];
+  var t=[];
+  var r = 350;
+  var c=2*Math.PI*r;
+  var l=c/100;
+  l=Math.floor(l);
+  console.log(l);
+  var style = { font: "16px Courier", fill: "#000000" };
   var theta=0;
-  for (i = 0; i < 0; i++) {
-           var x = 350 * Math.cos(alpha - theta);
-           var y = 350 * Math.sin(alpha - theta);
-           rot_image = rotateAndCache(base_image, theta)
-           ctx.drawImage(rot_image, x, y, 18, 18);
-           var x2 = 375 * Math.cos(alpha - theta);
-           var y2 = 375 * Math.sin(alpha - theta);
-           ctx.font = "16px Source Sans Pro";
-           ctx.fillText(i+1,x2,y2);
-           theta += Math.PI/50;
-       }
-
-  console.log(test);
-  console.log(test.scale);
-
-
-    cursors = game.input.keyboard.createCursorKeys();
-}
-
-function createText() {
-    // text = game.add.text(game.world.centerX, game.world.centerY, "google web fonts demo");
-    // text.font = 'Source Sans Pro';
-    // text.fontSize = 16;
-    // //  x0, y0 - x1, y1
-    // text.align = 'center';
-    // text.inputEnabled = true;
-    // text.input.enableDrag();
-    // text.events.onInputOver.add(over, this);
-    // text.events.onInputOut.add(out, this);
-}
-
-function out() {
-}
-
-function over() {
-    text.fill = '#ff00ff';
+  for (i = 0; i < 100; i++) {
+    var x = r*Math.cos(Math.PI/2+Math.PI*theta/180);
+    var y = r*Math.sin(Math.PI/2+Math.PI*theta/180);
+    s[i]=game.add.sprite(xplus+x, yplus+y, 'chair');
+    s[i].scale.setTo(1/30, 1/30);
+    s[i].anchor.set(0.5,0.5);
+    s[i].x=xplus+x;
+    s[i].y=yplus+y;
+    // s[i].angle+=(theta*180/Math.PI);
+    s[i].angle+=theta;
+    var x2 = (r+20)*Math.cos(Math.PI/2+Math.PI*theta/180);
+    var y2 = (r+20)*Math.sin(Math.PI/2+Math.PI*theta/180);
+    t[i] = game.add.text(xplus+x2, yplus+y2, i, style);
+    t[i].anchor.set(0.5,0.5);
+    t[i].x=xplus+x2;
+    t[i].y=yplus+y2;
+    // t[i].angle+=theta;
+    // t[i].alignTo(s[i], Phaser.BOTTOM_CENTER, 0,0);
+    theta+=3.6;
+  }
 }
 
 function update() {
-
-    if (cursors.up.isDown)
-    {
-        if (cursors.up.shiftKey)
-        {
-            // d.angle++;
-        }
-        else
-        {
-            game.camera.y -= 4;
-        }
-    }
-    else if (cursors.down.isDown)
-    {
-        if (cursors.down.shiftKey)
-        {
-            // d.angle--;
-        }
-        else
-        {
-            game.camera.y += 4;
-        }
-    }
-
-    if (cursors.left.isDown)
-    {
-        if (cursors.left.shiftKey)
-        {
-            game.world.rotation -= 0.05;
-        }
-        else
-        {
-            game.camera.x -= 4;
-        }
-    }
-    else if (cursors.right.isDown)
-    {
-        if (cursors.right.shiftKey)
-        {
-            game.world.rotation += 0.05;
-        }
-        else
-        {
-            game.camera.x += 4;
-        }
-    }
-
 }
 
 function render() {
-
-    // game.debug.cameraInfo(game.camera, 32, 32);
-
 }
