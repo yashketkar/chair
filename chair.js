39cc500e7a88f16cc79b37b5e9f0575b84bcd59a
Yash Ketkar <yashketkar@hotmail.com>
2017-10-21 12:44:46 -0500

    Update js formatting

--- chair.js
+++ chair.js
@@ -1,4 +1,7 @@
-var game = new Phaser.Game(800, 800, Phaser.CANVAS, 'phaser-div', { preload: preload, create: create});
+var game = new Phaser.Game(800, 800, Phaser.CANVAS, 'phaser-div', {
+    preload: preload,
+    create: create
+});
 var runButtonText = document.getElementById("runButton").firstChild;
 var numberOfChairs = document.getElementById("numberOfChairs");
 var intervalAmount = document.getElementById("intervalAmount");
@@ -7,7 +10,9 @@ var interval = 200;
 var index;
 var count;
 var previousEvent;
-var chairs = Array.apply(null, Array(N)).map(function (_, i) {return i+1;});
+var chairs = Array.apply(null, Array(N)).map(function(_, i) {
+    return i + 1;
+});
 var running = false;
 
 function preload() {
@@ -15,92 +20,98 @@ function preload() {
 }
 
 function create() {
-  game.stage.backgroundColor = "#FFFFFF";
-  drawChairs(100);
+    game.stage.backgroundColor = "#FFFFFF";
+    drawChairs(100);
 }
 
 function drawChairs(N) {
-  var r = 350;
-  var xplus = 400;
-  var yplus = 400;
-  var s=[];
-  var t=[];
-  var l=2*Math.PI*r/N;
-  l=Math.floor(l)-1;
-  var style = { font: 'Source Sans Pro', fontWeight: 300, fontSize: '16px', fill: "#000000" };
-  var theta=0;
-  for (i = 0; i < N; i++) {
-    var x = r*Math.cos(Math.PI/2+Math.PI*theta/180);
-    var y = r*Math.sin(Math.PI/2+Math.PI*theta/180);
-    s[i]=game.add.sprite(xplus+x, yplus+y, 'chair');
-    // s[i].scale.setTo(l/600, l/600);
-    s[i].scale.setTo(1/30, 1/30);
-    s[i].anchor.set(0.5,0.5);
-    s[i].x=xplus+x;
-    s[i].y=yplus+y;
-    s[i].angle+=theta;
-    var x2 = (r+20)*Math.cos(Math.PI/2+Math.PI*theta/180);
-    var y2 = (r+20)*Math.sin(Math.PI/2+Math.PI*theta/180);
-    t[i] = game.add.text(xplus+x2, yplus+y2, chairs[i], style);
-    t[i].anchor.set(0.5,0.5);
-    t[i].x=xplus+x2;
-    t[i].y=yplus+y2;
-    theta-=360/N;
-  }
+    var r = 350;
+    var xplus = 400;
+    var yplus = 400;
+    var s = [];
+    var t = [];
+    var l = 2 * Math.PI * r / N;
+    l = Math.floor(l) - 1;
+    var style = {
+        font: 'Source Sans Pro',
+        fontWeight: 300,
+        fontSize: '16px',
+        fill: "#000000"
+    };
+    var theta = 0;
+    for (i = 0; i < N; i++) {
+        var x = r * Math.cos(Math.PI / 2 + Math.PI * theta / 180);
+        var y = r * Math.sin(Math.PI / 2 + Math.PI * theta / 180);
+        s[i] = game.add.sprite(xplus + x, yplus + y, 'chair');
+        // s[i].scale.setTo(l/600, l/600);
+        s[i].scale.setTo(1 / 30, 1 / 30);
+        s[i].anchor.set(0.5, 0.5);
+        s[i].x = xplus + x;
+        s[i].y = yplus + y;
+        s[i].angle += theta;
+        var x2 = (r + 20) * Math.cos(Math.PI / 2 + Math.PI * theta / 180);
+        var y2 = (r + 20) * Math.sin(Math.PI / 2 + Math.PI * theta / 180);
+        t[i] = game.add.text(xplus + x2, yplus + y2, chairs[i], style);
+        t[i].anchor.set(0.5, 0.5);
+        t[i].x = xplus + x2;
+        t[i].y = yplus + y2;
+        theta -= 360 / N;
+    }
 }
 
 function runSimulation() {
-    if (runButtonText.data=="Run Simulation" && running == true){
+    if (runButtonText.data == "Run Simulation" && running == true) {
         runButtonText.data = "Pause Simulation";
         game.time.events.resume();
-    }
-    else if (runButtonText.data == "Pause Simulation" &&running==true) {
+    } else if (runButtonText.data == "Pause Simulation" && running == true) {
         runButtonText.data = "Run Simulation";
         game.time.events.pause();
     } else {
-    updateN(numberOfChairs.value);
-    index=0;
-    count=1;
-    runButtonText.data = "Pause Simulation";
-    game.time.events.remove(previousEvent);
-    previousEvent = game.time.events.repeat(interval, chairs.length-1, removeChair, this);
-    running = true;
+        updateN(numberOfChairs.value);
+        index = 0;
+        count = 1;
+        runButtonText.data = "Pause Simulation";
+        game.time.events.remove(previousEvent);
+        previousEvent = game.time.events.repeat(interval, chairs.length - 1, removeChair, this);
+        running = true;
     }
 }
 
 function removeChair() {
     game.world.removeAll();
-    N-=1;
+    N -= 1;
     chairs.splice(index, 1);
-    index+=count;
-    index=index%chairs.length;
-    count+=1;
+    index += count;
+    index = index % chairs.length;
+    count += 1;
     drawChairs(N);
-    if(N==1){
+    if (N == 1) {
         running = false;
         runButtonText.data = "Run Simulation";
     }
 }
 
 function updateN(newN) {
-    N=parseInt(newN);
-    chairs = Array.apply(null, Array(N)).map(function (_, i) {return i+1;});
+    N = parseInt(newN);
+    chairs = Array.apply(null, Array(N)).map(function(_, i) {
+        return i + 1;
+    });
     game.time.events.removeAll();
     game.world.removeAll();
-    running=false;
+    running = false;
     runButtonText.data = "Run Simulation";
     drawChairs(N);
 }
 
 function updateInterval(newInterval) {
     interval = newInterval;
-    if(running){
+    if (running) {
         game.time.events.remove(previousEvent);
-        previousEvent = game.time.events.repeat(interval, chairs.length-1, removeChair, this);
+        previousEvent = game.time.events.repeat(interval, chairs.length - 1, removeChair, this);
     }
 }
 
-function resetState(){
+function resetState() {
     numberOfChairs.value = 100;
     updateN(100);
     intervalAmount.value = 200;
